e212053c07228a70ca54e17e721a7778a42b4d56
SteffanBurnette <burnettesteffansep7@gmail.com>
2026-01-11 23:25:33 -0500

    oop

---
+++ src/myFiles.java
@@ -0,0 +1,15 @@
+import java.io.FileWriter;
+
+public class myFiles {
+
+    public static void main(String [] args){
+
+        //How to write to a file
+
+        //FileWriter - Good for small or medium-sized text files
+        //BufferedWriter - Better performance for large amounts of text
+        //PrintWriter - Best for structured data, like reports or logs
+        //FileOutputStream - Best for binary files (e.g. images, audio files)
+
+    }
+}
